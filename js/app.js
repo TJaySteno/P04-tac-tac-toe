a4bcba71804c5310aa8184166fec8f2d2d11e156
Trent <trent.stenoien@gmail.com>
2018-03-18 03:42:07 -0500

    Organized code, added comments, and 1 bugfix

--- js/app.js
+++ js/app.js
@@ -1,7 +1,84 @@
+/************************************************
+  START AND WIN SCREENS
+************************************************/
+
+// Create start screen and assign 'click' listener to 'Start game' button
+const $startScreen = $(`
+    <div class="screen screen-start" id="start">
+      <header>
+        <h1>Tic Tac Toe</h1>
+        <a href="#" class="button">Start game</a>
+      </header>
+    </div>
+  `);
+$startScreen.find('a').click(() => {
+  $startScreen.hide();
+  $('#board').show();
+  $('#player1').addClass('active');
+});
+
+// Create win screen and assign 'click' listener to 'New game' button. This listener also resets win screen.
+const $winScreen = $(`
+    <div class="screen screen-win" id="finish">
+      <header>
+        <h1>Tic Tac Toe</h1>
+        <p class="message">Winner</p>
+        <a href="#" class="button">New game</a>
+      </header>
+    </div>
+  `).hide();
+$winScreen.find('a').click(() => {
+  $winScreen.hide().find('p').text('Winner');
+  $winScreen.removeClass('screen-win-tie')
+            .removeClass('screen-win-one')
+            .removeClass('screen-win-two');
+  $('#start').show();
+});
+
+// Hide game board, then insert start and win screens before and after
+$('#board').hide().before($startScreen).after($winScreen);
+
+
+
+/************************************************
+  BOARD SCREEN - BASIC
+************************************************/
+
+// Store game board in variable and give its children unique names
+const $boxes = $('.boxes');
+$boxes.children().each(function (i) { $(this).prop('name', i + 1) });
+
+// Check clicked boxes and test against winning patterns; on victory, loads win screen
+const checkForWin = player => {
+  const checkedBoxes = findCheckedBoxes(player);
+  if (checkedBoxes.length === 5) loadVictory('cat');
+  else if (testCheckedBoxes(checkedBoxes)) loadVictory(player);
+}
+
+// Combine the names of a player's boxes into a string
+const findCheckedBoxes = player => {
+  let message = '';
+  if (player === 'player1') $boxes.find('.box-filled-1').each(function () { message += this.name });
+  else $boxes.find('.box-filled-2').each(function () { message += this.name });
+  return message;
+}
+
+// Test a string representing checked boxes against winning patterns
+const testCheckedBoxes = checkedBoxes => {
+  if ( /123/.test(checkedBoxes)
+    || /456/.test(checkedBoxes)
+    || /789/.test(checkedBoxes)
+    || /1\d*4\d*7/.test(checkedBoxes)
+    || /2\d*5\d*8/.test(checkedBoxes)
+    || /3\d*6\d*9/.test(checkedBoxes)
+    || /1\d*5\d*9/.test(checkedBoxes)
+    || /3\d*5\d*7/.test(checkedBoxes) ) return true;
+}
+
+// Resets the board, then loads the victor's win screen
 const loadVictory = player => {
   $(`#player2`).removeClass('active');
-  console.log($(`#${player}`).hasClass('active'));
-  $('.boxes').children().each(function () {
+  $boxes.children().each(function () {
     $(this).removeClass('box-filled-1')
            .removeClass('box-filled-2')
            .css('background-image', 'none');
@@ -17,97 +94,37 @@ const loadVictory = player => {
   $victoryScreen.show();
 }
 
-// Test a checkedBoxes string against winning patterns
-const playerWins = checkedBoxes => {
-  if ( /123/.test(checkedBoxes)
-    || /456/.test(checkedBoxes)
-    || /789/.test(checkedBoxes)
-    || /1\d*4\d*7/.test(checkedBoxes)
-    || /2\d*5\d*8/.test(checkedBoxes)
-    || /3\d*6\d*9/.test(checkedBoxes)
-    || /1\d*5\d*9/.test(checkedBoxes)
-    || /3\d*5\d*7/.test(checkedBoxes) ) return true;
-}
+// On mouseover of a box not yet selected, display an O or X
+$boxes.mouseover(e => {
+  if (!$(e.target).hasClass('box-filled-1')
+  &&  !$(e.target).hasClass('box-filled-2')) {
+    const player = $('.active').prop('id');
+    if (player === 'player1') $(e.target).css('background-image', "url('img/o.svg')");
+    if (player === 'player2') $(e.target).css('background-image', "url('img/x.svg')");
+  }
+});
 
-// Check clicked boxes and test against winning patterns; on victory, loads win screen
-const checkForWin = player => {
-  let checkedBoxes = function () {
-    let message = '';
-    if (player === 'player1') $('.boxes').find('.box-filled-1').each(function () { message += this.name });
-    else $('.boxes').find('.box-filled-2').each(function () { message += this.name });
-    return message;
-  }();
-  console.log(checkedBoxes.length);
-  if (checkedBoxes.length === 5) loadVictory('cat');
-  else if (playerWins(checkedBoxes)) loadVictory(player);
-}
+// On mouseout from a box not yet selected, remove an O or X
+$boxes.mouseout(e => {
+  if (!$(e.target).hasClass('box-filled-1')
+  &&  !$(e.target).hasClass('box-filled-2')) $(e.target).css('background-image', 'none');
+});
 
-(() => {
-  // Store board and assign unique names to each box
-  const $boxes = $('.boxes');
+// When a box not yet selected is clicked, add a class to that box, indicate the next player's turn, then check for victory
+$boxes.click(e => {
+  const player = $('.active').prop('id');
+  $('.active').removeClass('active');
 
-  $boxes.children().each(function (i) { $(this).prop('name', i + 1) });
-
-  $boxes.mouseover(e => {
-    if (!$(e.target).hasClass('box-filled-1')
-    &&  !$(e.target).hasClass('box-filled-2')) {
-      const player = $('.active').prop('id');
-      if (player === 'player1') $(e.target).css('background-image', "url('img/o.svg')");
-      if (player === 'player2') $(e.target).css('background-image', "url('img/x.svg')");
+  if (!$(e.target).hasClass('box-filled-1')
+  &&  !$(e.target).hasClass('box-filled-2')) {
+    if (player === 'player1') {
+      $(e.target).addClass('box-filled-1');
+      $('#player2').addClass('active');
+    } else {
+      $(e.target).addClass('box-filled-2');
+      $('#player1').addClass('active');
     }
-  });
 
-  $boxes.mouseout(e => {
-    if (!$(e.target).hasClass('box-filled-1')
-    &&  !$(e.target).hasClass('box-filled-2')) $(e.target).css('background-image', 'none');
-  });
-
-  $boxes.click(e => {
-    if (!$(e.target).hasClass('box-filled-1')
-    &&  !$(e.target).hasClass('box-filled-2')) {
-      const player = $('.active').prop('id');
-      $('.active').removeClass('active');
-
-      if (player === 'player1') {
-        $(e.target).addClass('box-filled-1');
-        $('#player2').addClass('active');
-      } else {
-        $(e.target).addClass('box-filled-2');
-        $('#player1').addClass('active');
-      }
-
-      checkForWin(player);
-    }
-  });
-
-  const $startScreen = $(`
-      <div class="screen screen-start" id="start">
-        <header>
-          <h1>Tic Tac Toe</h1>
-          <a href="#" class="button">Start game</a>
-        </header>
-      </div>
-    `);
-  $startScreen.find('a').click(() => {
-    $startScreen.hide();
-    $('#board').show();
-    $('#player1').addClass('active');
-  });
-
-  const $winScreen = $(`
-      <div class="screen screen-win" id="finish">
-        <header>
-          <h1>Tic Tac Toe</h1>
-          <p class="message">Winner</p>
-          <a href="#" class="button">New game</a>
-        </header>
-      </div>
-    `).hide();
-  $winScreen.find('a').click(() => {
-    $winScreen.hide().find('p').text('Winner');
-    $('#start').show();
-  });
-
-  $('#board').hide().before($startScreen).after($winScreen);
-
-})()
+    checkForWin(player);
+  }
+});
